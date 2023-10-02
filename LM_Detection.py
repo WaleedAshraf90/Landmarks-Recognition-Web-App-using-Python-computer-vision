01a5b78162ff4ad93f7aa41fcf95b7ee062d8195
Waleed Ashraf <114513424+WaleedAshraf90@users.noreply.github.com>
2023-10-02 16:47:32 +0300

    Add files via upload

---
+++ LM_Detection.py
@@ -0,0 +1,67 @@
+import streamlit as st
+import PIL
+import tensorflow as tf
+import tensorflow_hub as hub
+import numpy as np
+import pandas as pd
+from geopy.geocoders import Nominatim
+
+model_url = 'https://tfhub.dev/google/on_device_vision/classifier/landmarks_classifier_africa_V1/1'
+# model_url = 'on_device_vision_classifier_landmarks_classifier_asia_V1_1'
+
+# label_url = 'https://www.gstatic.com/aihub/tfhub/labelmaps/landmarks_classifier_asia_V1_label_map.csv'
+labels = 'landmarks_classifier_africa_V1_label_map.csv'
+df = pd.read_csv(labels)
+labels = dict(zip(df.id, df.name))
+
+def image_processing(image):
+    img_shape = (321, 321)
+    classifier = tf.keras.Sequential(
+        [hub.KerasLayer(model_url, input_shape=img_shape + (3,), output_key="predictions:logits")]) # بجيبة عن طريق tensorflowhub 
+    img = PIL.Image.open(image)
+    img = img.resize(img_shape)
+    img1 = img  # خدت نسخة علشان اعمل visulaztion
+    img = np.array(img) / 255.0
+    img = img[np.newaxis]
+    result = classifier.predict(img) #علشان الموديل يقدر يفهم الصورة
+    return labels[np.argmax(result)],img1  # argmax بيدينى احتمالية
+    # بروح ابدلة من label علشان ارجع class name
+
+
+
+# function caluct longitude and latitude
+def get_map(loc):    
+    geolocator = Nominatim(user_agent="Your_Name")
+    location = geolocator.geocode(loc)
+    return location.address,location.latitude, location.longitude
+
+
+
+
+# لازم احط الصورة بتاعى على الجهاز علشان الموديل يقدر يشوفها
+def run():
+    st.title("Landmark Recognition")
+    img = PIL.Image.open('logo.png')
+    img = img.resize((256,256))
+    st.image(img)
+    img_file = st.file_uploader("Choose your Image", type=['png', 'jpg'])
+    if img_file is not None:
+        save_image_path = './Uploaded_Images/' + img_file.name  # هنا بحدد path علشان الصورة يقدر يرفعها علشان لما الصورة بتترفع على الموقع بتتخزن هنا علشان اقدر الموديل يعملها classification
+        with open(save_image_path, "wb") as f:
+            f.write(img_file.getbuffer())
+        prediction,image = image_processing(save_image_path)
+        st.image(image)
+        st.header("📍 **Predicted Landmark is: " + prediction + '**')
+        try:
+            address, latitude, longitude = get_map(prediction)
+            st.success('Address: '+address )
+            loc_dict = {'Latitude':latitude,'Longitude':longitude}
+            st.subheader('✅ **Latitude & Longitude of '+prediction+'**')
+            st.json(loc_dict)
+            data = [[latitude,longitude]]
+            df = pd.DataFrame(data, columns=['lat', 'lon'])
+            st.subheader('✅ **'+prediction +' on the Map**'+'🗺️')
+            st.map(df)
+        except Exception as e:
+            st.warning("No address found!!")
+run()
